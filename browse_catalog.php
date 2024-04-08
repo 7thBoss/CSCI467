b57927fde682fbf3427ae93a87003036ffb51fb1
7thBoss <cameronmhaines@gmail.com>
2024-04-07 19:14:42 -0500

    browse_catalog_functionality

--- browse_catalog.php
+++ browse_catalog.php
@@ -1,11 +1,13 @@
 <?php
 include "functions.php";
 
+//Get parts to browse
 $results = legacy_sql_query("SELECT * FROM parts");
 
+//Display parts
 foreach($results as $result)
 {
-	echo $result["number"]."\n";	
+	echo $result["number"]." ".$result["description"]." ".$result["price"]." ".$result["weight"]." ".$result["pictureURL"]."<br>";	
 }
 
 ?>
