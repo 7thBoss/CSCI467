5dc2b9f92bcc4daf4b6005a9f9234c15584262f3
YudishSheth <157249426+YudishSheth@users.noreply.github.com>
2024-04-07 22:48:40 -0500

    Update mock_db_setup.sql

--- mock_db_setup.sql
+++ mock_db_setup.sql
@@ -1,25 +1,16 @@
-CREATE TABLE legacy ( 
-  part_num INT(64) NOT NULL, 
-  description CHAR(64) NOT NULL, 
-  weight INT(64) NOT NULL, 
-  picture CHAR(64), 
-  price FLOAT(64) NOT NULL, 
-  PRIMARY KEY (part_num));
-
-CREATE TABLE warehouse ( 
-  warehouse_id INT(64) NOT NULL, 
-  part_num INT(64) NOT NULL, 
-  quantity INT(64), PRIMARY KEY (warehouse_id, part_num), 
-  FOREIGN KEY (part_num) REFERENCES legacy(part_num));
-
 CREATE TABLE order_parts ( 
   order_id INT(64) NOT NULL,
   part_num INT(64) NOT NULL,
   quantity INT(64) NOT NULL,
   PRIMARY KEY (customer_id, part_num));
 
-CREATE TABLE order ( 
+CREATE TABLE orders ( 
   customer_id INT(64) NOT NULL,
   order_id INT(64) NOT NULL,
   PRIMARY KEY (customer_id, order_id)
   FOREIGN KEY (order_id) REFERENCES order_parts(order_id));
+
+CREATE TABLE warehouses ( 
+  warehouse_id INT(64) NOT NULL, 
+  part_num INT(64) NOT NULL, 
+  quantity INT(64), PRIMARY KEY (warehouse_id, part_num));
