3df2a09cd8936d9f69a08013f053eefeaa624153
YudishSheth <157249426+YudishSheth@users.noreply.github.com>
2024-04-14 22:59:46 -0500

    Update db_setup.sql

--- db_setup.sql
+++ db_setup.sql
@@ -1,17 +1,20 @@
 CREATE TABLE orders ( 
-  order_id INT(64) NOT NULL AUTO_INCREMENT,
-  customer_id INT(64) NOT NULL,
-  order_status CHAR(20) NOT NULL,
-  PRIMARY KEY (order_id));
+    order_id INT(8) NOT NULL AUTO_INCREMENT,
+    customer_id INT(32) NOT NULL,
+    order_status VARCHAR(8) NOT NULL,
+    PRIMARY KEY (order_id)
+);
 
 CREATE TABLE order_parts ( 
-  part_num INT(64) NOT NULL,
-  order_id INT(64) NOT NULL,
-  quantity INT(64) NOT NULL,
-  PRIMARY KEY (part_num),
-  FOREIGN KEY (order_id) REFERENCES orders(order_id));
+    part_num INT(11) NOT NULL,
+    order_id INT(11) NOT NULL,
+    quantity INT(32) NOT NULL,
+    PRIMARY KEY (part_num, order_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
 
 CREATE TABLE warehouse_parts ( 
-  part_num INT(64) NOT NULL, 
-  quantity INT(64) NOT NULL, 
-  PRIMARY KEY (part_num));
+    part_num INT(11) NOT NULL,
+    onhand INT(32) NOT NULL,
+    PRIMARY KEY (part_num)
+);
