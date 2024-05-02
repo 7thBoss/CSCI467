77763c6746547efc62eed00fedda1815df414377
YudishSheth <157249426+YudishSheth@users.noreply.github.com>
2024-05-02 15:14:51 -0500

    Add files via upload

---
+++ format.php
@@ -0,0 +1,54 @@
+<?php
+
+echo "
+<html>
+    <head>
+		<title>Packing List</title>
+		<meta charset='UTF-8'/>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<style>
+			header
+			{
+				padding: 1px;
+				width: 100%;
+				background-color: white;
+			}
+			table
+			{
+				margin: 40px;
+				// margin-left: auto;
+				// margin-right: auto;
+				// width: 50%;
+				background-color: white;
+				border-collapse: collapse;
+			}
+			th
+			{
+				text-align: center;
+				height: 40px;
+				width: 120px;
+			}
+			td
+			{
+				// text-align: center;
+				border: 2px solid black;
+			}
+			body
+			{
+				margin: 0;
+				padding: 0;
+				background-attachment: fixed;
+				background-image: linear-gradient(-50deg, lightblue, lightblue); 
+			}
+			h1
+			{
+				padding-left:40px;
+			}
+		</style>
+		<header>
+			<h1>Packing List</h1>
+		</header>
+    </head>
+<html>";
+
+?>
